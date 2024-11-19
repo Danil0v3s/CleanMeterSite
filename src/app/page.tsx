aafbe9ca021bc45be40b1cf5e6837a4b1c0737aa
Danilo Lemes <daniloleemes@hotmail.com>
2024-11-19 19:49:18 +0000

    site

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,97 +3,108 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+      <div className="absolute flex aspect-[9/16] h-full md:aspect-video">
+        <iframe
+          className="aspect-[9/16] h-full md:aspect-video"
+          src="https://www.youtube.com/embed/G79McCEQCQg?si=FRZy5iNukuRqs8gE&autoplay=1&mute=1&loop=1&rel=0&showinfo=0&controls=0&color=white&iv_load_policy=3&enablejsapi=1&hd=1&vq=hd1080&playlist=G79McCEQCQg"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+        />
+        <div className="aspect-[9/16] h-full md:aspect-video absolute backdrop-brightness-75" />
+      </div>
+      <div className="absolute z-1 size-full grid-bg" />
+
+      <main className="flex flex-col gap-20 row-start-2 items-center relative z-2 p-8">
         <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
+          src="/logo.png"
+          alt="Clean Meter logo"
+          width={150}
+          height={150}
           priority
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
+        <div className="flex flex-col text-center gap-2">
+          <span>INTRODUCING</span>
+          <h1>Clean Meter</h1>
+          <div>
+            <p>Minimalistic performance monitor to track</p>
+            <p>your system's stats in real-time</p>
+          </div>
+        </div>
+
+        <div className="flex gap-4 items-center flex-col">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            href="https://github.com/Danil0v3s/CleanMeter/releases/latest/download/cleanmeter.windows.zip"
             target="_blank"
             rel="noopener noreferrer"
           >
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
+              src="/download.svg"
+              alt="Download icon"
               width={20}
               height={20}
             />
-            Deploy now
+            Download now
           </a>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            href="https://discord.gg/phqwe89cvE"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read our docs
+            Join our community
           </a>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center relative z-1 pb-16">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="https://www.paypal.com/donate/?hosted_button_id=W2GU6AHGQUND8"
           target="_blank"
           rel="noopener noreferrer"
         >
           <Image
             aria-hidden
-            src="/file.svg"
-            alt="File icon"
+            src="/paypal.svg"
+            alt="Paypal icon"
             width={16}
             height={16}
           />
-          Learn
+          Donate via PayPal
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="https://ko-fi.com/danil0v3s"
           target="_blank"
           rel="noopener noreferrer"
         >
           <Image
             aria-hidden
-            src="/window.svg"
-            alt="Window icon"
+            src="/kofi_symbol.svg"
+            alt="Ko-Fi icon"
             width={16}
             height={16}
           />
-          Examples
+          Donate via Ko-Fi
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="https://github.com/Danil0v3s/CleanMeter"
           target="_blank"
           rel="noopener noreferrer"
         >
           <Image
             aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
+            src="/github-mark-white.svg"
+            alt="GitHub icon"
             width={16}
             height={16}
           />
-          Go to nextjs.org →
+          Source code
         </a>
       </footer>
     </div>
