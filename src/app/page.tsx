e5258538e54f274eaf0a51081aeb2690d3b694b2
Danilo Lemes <daniloleemes@hotmail.com>
2024-11-24 18:00:30 +0000

    fix layouts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="flex flex-col justify-center h-screen">
       <div className="absolute flex aspect-[9/16] h-full md:aspect-video">
         <iframe
           className="aspect-[9/16] h-full md:aspect-video"
@@ -16,7 +16,7 @@ export default function Home() {
       </div>
       <div className="absolute z-1 size-full grid-bg" />
 
-      <main className="flex flex-col gap-20 row-start-2 items-center relative z-2 p-8">
+      <main className="flex flex-col gap-8 md:gap-20 row-start-2 items-center relative z-2 p-8">
         <Image
           src="/logo.png"
           alt="Clean Meter logo"
@@ -60,7 +60,7 @@ export default function Home() {
         </div>
       </main>
 
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center relative z-1 pb-16">
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center z-1 relative">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://www.paypal.com/donate/?hosted_button_id=W2GU6AHGQUND8"
